ecab79d3fab7291a0af28abd966cd3d9a1f79eb8
nora-alaa <nora.alaa.salama@gmail.com>
2022-04-26 23:47:00 +0200

    فثسف

--- TechStore2/src/main/java/database/MongoDBHandler.java
+++ TechStore2/src/main/java/database/MongoDBHandler.java
@@ -4,49 +4,47 @@
  */
 package database;
 
-import com.mongodb.*;
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCursor;
 import java.util.ArrayList;
 import java.util.List;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+
 /**
  *
  * @author Omar Samir
  */
 public class MongoDBHandler {
 
-    public static List<Order>  reviews = new ArrayList<Order>();
+    private static String dataBaseUrl = "mongodb://uyqf3ppxafmtrb3gjwg5:l1xjRgKBIGQJipS8lbE4@bdwkbvjqqwvfykg-mongodb.services.clever-cloud.com:27017/bdwkbvjqqwvfykg";
+    private static String dataBaseName = "bdwkbvjqqwvfykg";
+
+    public static List<Order> reviews = new ArrayList<Order>();
     public static MongoClient mongoClient;
     public static MongoCollection<Order> collection;
     public static MongoCursor<Order> cursor;
-    //public static Date date = Calendar.getInstance().getTime();
-    //public static DateFormat dateFormat = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
-    //public static String strDate = dateFormat.format(date);
 
     public static void connectDB() {
 
-// Creating a Mongo client 
-      MongoClient mongo = new MongoClient( "localhost" , 27017 ); 
-   
-      // Creating Credentials 
-      MongoCredential credential; 
-      credential = MongoCredential.createCredential("nora", "newDB", 
-         "1234".toCharArray()); 
-      System.out.println("Connected to the database successfully");  
-      
-      // Accessing the database 
-      MongoDatabase database = mongo.getDatabase("newDB"); 
-      System.out.println("Credentials ::"+ credential);
-      
-      //Creating a collection 
-      database.createCollection("sampleCollection"); 
-      System.out.println("Collection created successfully"); 
+        // Creating a Mongo client 
+//        MongoClient mongo = new MongoClient(dataBaseUrl, 27017);
+        MongoClient mongoClient = new MongoClient(new MongoClientURI(dataBaseUrl));
+
+        System.out.println("Connected to the database successfully");
+
+        // Accessing the database 
+        MongoDatabase database = mongoClient.getDatabase(dataBaseName);
+
+        //Creating a collection 
+//        database.createCollection("sampleCollection");
+        for (String name : database.listCollectionNames()) {
+
+            System.out.println(name);
+        }
+        System.out.println("Collection created successfully");
 
     }
 
@@ -58,5 +56,4 @@ public class MongoDBHandler {
         return reviews;
     }
 
-    
 }
